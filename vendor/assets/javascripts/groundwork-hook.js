1b5652c77d62d427eba920e397b447d6ef173191
Ognjen Regoje <ognjen.regoje@gmail.com>
2013-03-27 10:51:57 +0800

    Update resources. Patch bump.

--- vendor/assets/javascripts/groundwork-hook.js
+++ vendor/assets/javascripts/groundwork-hook.js
@@ -1,3 +1,5 @@
 //= require_tree ./groundworkcss/libs
+//= require ./groundworkcss/plugins/jquery.cycle2.js
+//= require_tree ./groundworkcss/plugins/cycle
 //= require_tree ./groundworkcss/plugins
 //= require ./groundworkcss/groundwork.all.js
