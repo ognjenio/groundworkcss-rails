f9fb47092574de9bb4411e83130e3427aaaf9a2a
Ognjen Regoje <ognjen.regoje@gmail.com>
2013-04-18 11:17:08 +0800

    Init. resources from scratch.
    Patch bump.

--- vendor/assets/javascripts/groundworkcss/components/responsiveText.js
+++ vendor/assets/javascripts/groundworkcss/components/responsiveText.js
@@ -5,17 +5,34 @@
 
 $(function() {
   return $('.responsive').not('table').each(function(index, object) {
-    var compression, max, min;
+    var $this, compression, max, min, scrollReset, scrollTime;
     compression = 10;
     min = 10;
     max = 200;
+    scrollTime = 650;
+    scrollReset = 200;
     compression = parseFloat($(this).attr('data-compression') || compression);
     min = parseFloat($(this).attr('data-min') || min);
     max = parseFloat($(this).attr('data-max') || max);
-    return $(object).responsiveText({
+    $(object).responsiveText({
       compressor: compression,
       minSize: min,
       maxSize: max
+    });
+    $this = $(this);
+    return $(this).hover((function() {
+      var difference;
+      difference = $this.get(0).scrollWidth - $this.width();
+      console.log(difference);
+      if (difference > 0) {
+        return $this.stop().animate({
+          "text-indent": -difference
+        }, scrollTime);
+      }
+    }), function() {
+      return $this.stop().animate({
+        "text-indent": 0
+      }, scrollReset);
     });
   });
 });
